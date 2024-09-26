d19095dd7b26f35c759badefb6994eb56ed6e415
aswinsagar12 <aswinsagar12@gmail.com>
2024-09-26 18:32:47 +0530

    Feature Form Component

---
+++ weather/src/components/Form.jsx
@@ -0,0 +1,14 @@
+import { React } from 'react';
+
+const Form = () => {
+
+    return (<div>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <input type="weather" />
+    </div>
+    )
+}
+
+export default Form
