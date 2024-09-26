b9c51bbfd5736a1fc2e12f921422f23377b9d247
Aswin Sagar <87416215+aswinsagar12@users.noreply.github.com>
2024-09-26 19:45:09 +0530

    Feature Form and API Connection

--- weather/src/components/Form.jsx
+++ weather/src/components/Form.jsx
@@ -1,14 +1,32 @@
-import { React } from 'react';
-
+import { React } from "react";
+import { useState } from "react";
+import { axios } from "react";
 const Form = () => {
+  const [zipCode, setZipCode] = useState("");
 
-    return (<div>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <input type="weather" />
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setZipCode("")
+    console.log('Form has been submitted');
+
+    const weatherData = await axios.post('/', {zipCode})
+    console.log(weatherData.data);
+
+  };
+
+  return (
+    <div className="w-full h-[100vh] flex justify-center items-center">
+      <form onSubmit={handleSubmit} action="">
+      <input
+        className="border border-black rounded-md"
+        type="weather"
+        onChange={(e) => setZipCode(e.target.value)}
+        value={setZipCode}
+      />
+      <button className="rounded-md bg-green-500">Search</button>
+    </form>
     </div>
-    )
-}
+  );
+};
 
-export default Form
+export default Form;
