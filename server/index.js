b42ff3f8aea558d1176b41b293631c960376fbfc
Aswin Sagar <87416215+aswinsagar12@users.noreply.github.com>
2024-09-26 19:45:35 +0530

    Add Post method

--- server/index.js
+++ server/index.js
@@ -1,22 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors')
-const axios = require('axios');
-const bodyParser = require('body-parser')
-require('dotenv').config();
+const cors = require("cors");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 // Application port and HostName configuration
-const hostname = 'localhost';
+const hostname = "localhost";
 const PORT = process.env.PORT || 8080;
 
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.listen(PORT, hostname, ()=>{
-    console.log(`The server is running on ${hostname}:${PORT}`);
-})
+app.listen(PORT, hostname, () => {
+  console.log(`The server is running on ${hostname}:${PORT}`);
+});
 
-app.get('/', async(req,res)=>{
-    const weather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=20230,us&appid=${process.env.api_key}`);
-     res.send(weather.data)
-     console.log(weather.data);
- 
-})
+app.get("/", async (req, res) => {
+  const weather = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?zip=20230,us&appid=${process.env.api_key}`
+  );
+  res.send(weather.data);
+  console.log(weather.data);
+});
+
+app.post("/", async (req, res) => {
+    console.log(req.body);
+//   const weather = await axios.get(
+//     `https://api.openweathermap.org/data/2.5/weather?zip={zipCode},us&appid=${process.env.api_key}`
+//   );
+//   res.send(weather.data);
+//   console.log(weather.data);
+});
