adadcf1e3a9ab34cd20a9e0bb2b6aded32b3419f
Aswin Sagar <87416215+aswinsagar12@users.noreply.github.com>
2024-09-26 16:24:43 +0530

    Add Weather API updates

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
+const axios = require('axios');
+const bodyParser = require('body-parser')
+require('dotenv').config();
 
+// Application port and HostName configuration
 const hostname = 'localhost';
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
 
 app.listen(PORT, hostname, ()=>{
     console.log(`The server is running on ${hostname}:${PORT}`);
 })
 
+app.get('/', async(req,res)=>{
+    const weather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=20230,us&appid=${process.env.api_key}`);
+     res.send(weather.data)
+     console.log(weather.data);
+ 
+})
